272d1e0728e00bb4462139131275699592b72191
Tom Burniston <thomas.burniston@gmail.com>
2024-02-02 13:18:10 +0000

    Some p5js ts stuff

---
+++ Javascript/P5js/!TemplateTS/src/app/main.ts
@@ -0,0 +1,14 @@
+
+import p5 from "p5";
+
+export default (p: p5) => {
+  p.setup = () => {
+    const canvas = p.createCanvas(400, 400)
+    canvas.parent("app");
+  }
+
+  p.draw = () => {
+    p.background(220)
+    p.ellipse(p.width / 2, p.height / 2, 50, 50)
+  }
+}
---
+++ Javascript/P5js/[..]Other/ExampleTs/src/index.ts
@@ -0,0 +1,4 @@
+
+import p5 from "p5";
+import app from "./app/main";
+const a = new p5(app);
