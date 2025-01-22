60a84ded966a95b26f1861927ff4906555afc7d5
tomster12 <thomas.burniston@gmail.com>
2025-01-22 00:26:28 +0000

    Changed build script to batch script

--- c/collision/src/main.c
+++ c/collision/src/main.c
@@ -2,19 +2,38 @@
 
 int main(void)
 {
-    const int screenWidth = 600;
-    const int screenHeight = 450;
+    const int screenWidth = 500;
+    const int screenHeight = 500;
 
-    InitWindow(screenWidth, screenHeight, "Different Title");
+    InitWindow(screenWidth, screenHeight, "Title");
 
     SetTargetFPS(60);
+
+    Vector2 points[] = {
+        {100, 100},
+        {200, 100},
+        {200, 200},
+        {100, 200}};
+
+    int triangles[] = {
+        0,
+        2,
+        1,
+        0,
+        3,
+        2};
+
+    int triangleCount = sizeof(triangles) / sizeof(triangles[0]);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText("Hello, Raylib!", 50, 200, 20, LIGHTGRAY);
+        for (int i = 0; i < triangleCount; i += 3)
+        {
+            DrawTriangle(points[triangles[i]], points[triangles[i + 1]], points[triangles[i + 2]], DARKGRAY);
+        }
 
         EndDrawing();
     }
