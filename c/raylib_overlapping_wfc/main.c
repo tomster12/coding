d868f9f53ecbefda4a19b484b89610886e8c707c
Thomas Burniston <tom.burniston@outlook.com>
2025-01-31 20:04:40 +0000

    Implemented pattern extraction from source image

--- c/raylib_overlapping_wfc/main.c
+++ c/raylib_overlapping_wfc/main.c
@@ -8,57 +8,75 @@
 #define SCREEN_HEIGHT 800
 #define OUTPUT_DIM 128
 
-typedef struct PatternSet
+typedef struct WFCPatternSet
 {
     Color **pattern_colors;
     Texture2D *pattern_textures;
     size_t pattern_count;
     size_t pattern_dim;
-    size_t grid_dim;
-} PatternSet;
+    size_t source_width;
+    size_t source_height;
+} WFCPatternSet;
 
-typedef struct WaveOutput
+typedef struct WFCWave
 {
     int *data;
     size_t width;
     size_t height;
-} WaveOutput;
+} WFCWave;
 
-void pattern_set_init_from_image(PatternSet *pattern_set, char *image_path)
+void pattern_set_init_from_image(WFCPatternSet *pattern_set, char *input_image_path)
 {
-    Image input_image = LoadImage(image_path);
-    ImageFormat(&input_image, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+    Image source_image = LoadImage(input_image_path);
+    ImageFormat(&source_image, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+    Color *source_pixels = LoadImageColors(source_image);
+    UnloadImage(source_image);
 
-    pattern_set->grid_dim = input_image.width;
+    pattern_set->source_width = source_image.width;
+    pattern_set->source_height = source_image.height;
     pattern_set->pattern_dim = 3;
-    pattern_set->pattern_count = pattern_set->grid_dim * pattern_set->grid_dim;
+    pattern_set->pattern_count = pattern_set->source_width * pattern_set->source_height;
     pattern_set->pattern_colors = malloc((pattern_set->pattern_count) * sizeof(Color *));
     pattern_set->pattern_textures = malloc((pattern_set->pattern_count) * sizeof(Texture2D));
 
-    Color *pixels = LoadImageColors(input_image);
-
-    for (size_t y = 0; y < pattern_set->grid_dim; ++y)
+    for (size_t centre_y = 0; centre_y < pattern_set->source_width; ++centre_y)
     {
-        for (size_t x = 0; x < pattern_set->grid_dim; ++x)
+        for (size_t centre_x = 0; centre_x < pattern_set->source_height; ++centre_x)
         {
-            Image image = GenImageColor(pattern_set->pattern_dim, pattern_set->pattern_dim, BLUE);
-            Color *colors = LoadImageColors(image);
+            Image pattern_image = GenImageColor(pattern_set->pattern_dim, pattern_set->pattern_dim, PINK);
+            Color *pattern_colors = LoadImageColors(pattern_image);
+            Texture2D pattern_texture = LoadTextureFromImage(pattern_image);
+            UnloadImage(pattern_image);
 
-            // Create the pattern here
+            for (int pattern_x = 0; pattern_x < pattern_set->pattern_dim; ++pattern_x)
+            {
+                for (int pattern_y = 0; pattern_y < pattern_set->pattern_dim; ++pattern_y)
+                {
+                    size_t pixel_x = centre_x - pattern_set->pattern_dim / 2 + pattern_y;
+                    size_t pixel_y = centre_y - pattern_set->pattern_dim / 2 + pattern_x;
+                    Color pixel_color = WHITE;
 
-            int index = x + y * pattern_set->grid_dim;
-            pattern_set->pattern_colors[index] = colors;
-            pattern_set->pattern_textures[index] = LoadTextureFromImage(image);
+                    if (pixel_x >= 0 && pixel_x < pattern_set->source_width && pixel_y >= 0 && pixel_y < pattern_set->source_height)
+                    {
+                        pixel_color = source_pixels[pixel_x + pixel_y * pattern_set->source_width];
+                    }
 
-            UnloadImage(image);
+                    pattern_colors[pattern_y + pattern_x * pattern_set->pattern_dim] = pixel_color;
+                }
+            }
+
+            UpdateTexture(pattern_texture, pattern_colors);
+
+            int index = centre_x + centre_y * pattern_set->source_width;
+            pattern_set->pattern_colors[index] = pattern_colors;
+            pattern_set->pattern_textures[index] = pattern_texture;
         }
     }
 
-    UnloadImageColors(pixels);
-    UnloadImage(input_image);
+    UnloadImageColors(source_pixels);
 }
 
-void pattern_set_destroy(PatternSet *pattern_set)
+void pattern_set_destroy(WFCPatternSet *pattern_set)
 {
     for (size_t i = 0; i < pattern_set->pattern_count; ++i)
     {
@@ -70,35 +88,35 @@ void pattern_set_destroy(PatternSet *pattern_set)
     free(pattern_set->pattern_textures);
 }
 
-void pattern_set_draw(PatternSet *pattern_set)
+void pattern_set_draw(WFCPatternSet *pattern_set)
 {
     size_t gap = 10;
     size_t size = 50;
 
-    int start_x = SCREEN_WIDTH / 2 - (pattern_set->grid_dim * (gap + size) + size) / 2;
-    int start_y = SCREEN_HEIGHT / 2 - (pattern_set->grid_dim * (gap + size) + size) / 2;
+    int start_x = SCREEN_WIDTH / 2 - (pattern_set->source_width * (gap + size) + size) / 2;
+    int start_y = SCREEN_HEIGHT / 2 - (pattern_set->source_height * (gap + size) + size) / 2;
     size_t draw_size = size / pattern_set->pattern_dim;
 
-    for (size_t x = 0; x < pattern_set->grid_dim; ++x)
+    for (size_t x = 0; x < pattern_set->source_width; ++x)
     {
-        for (size_t y = 0; y < pattern_set->grid_dim; ++y)
+        for (size_t y = 0; y < pattern_set->source_height; ++y)
         {
-            size_t index = x + y * pattern_set->grid_dim;
+            size_t index = x + y * pattern_set->source_width;
             Vector2 draw_pos = {start_x + x * (size + gap), start_y + y * (size + gap)};
             DrawTextureEx(pattern_set->pattern_textures[index], draw_pos, 0.0, draw_size, WHITE);
         }
     }
 }
 
-void wave_init(WaveOutput *wave, size_t dim)
+void wave_init(WFCWave *wave, size_t dim)
 {
 }
 
-void wave_destroy(WaveOutput *wave)
+void wave_destroy(WFCWave *wave)
 {
 }
 
-void wave_propogate(WaveOutput *wave)
+void wave_propogate(WFCWave *wave)
 {
 }
 
@@ -107,10 +125,10 @@ int main()
     SetTraceLogLevel(LOG_WARNING);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Overlapping WFC");
 
-    PatternSet pattern_set;
+    WFCPatternSet pattern_set;
     pattern_set_init_from_image(&pattern_set, "../samples/City.png");
 
-    WaveOutput wave;
+    WFCWave wave;
     wave_init(&wave, OUTPUT_DIM);
 
     while (!WindowShouldClose())
