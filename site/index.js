fce440ecb9ce1e827fb0a751056e404531ccc248
tomster12 <thomas.burniston@gmail.com>
2025-03-15 00:55:27 +0000

    Updated website to a custom static site

---
+++ site/index.js
@@ -0,0 +1,99 @@
+const projects = {
+    "Wave Function Collapse": { url: "https://github.com/tomster12/coding/tree/master/c/raylib_overlapping_wfc", showcase: "/assets/gifs/c/WFC.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["c", "raylib"] },
+    "Liquid Simulation": { url: "https://github.com/tomster12/coding/tree/master/c++/Liquid", showcase: "/assets/gifs/c++/Liquid.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["c++", "sfml"] },
+    "Machine Learning (Implemented from scratch)": { url: "https://github.com/tomster12/machine-learning-cpp", showcase: "/assets/gifs/c++/MachineLearning.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["c++", "sfml"] },
+    "Entity Component System (ECS)": { url: "https://github.com/tomster12/coding/tree/master/c++/ECS", showcase: "/assets/gifs/c++/ECS.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["c++", "sfml"] },
+    "Collision 1D": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/collision_1D", showcase: "/assets/gifs/javascript/1DCollision.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Boids": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/boids_2D", showcase: "/assets/gifs/javascript/Boids.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Cave Generation": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/cave_generation", showcase: "/assets/gifs/javascript/CaveGeneration.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Dungeon Generation": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/dungeon_generation", showcase: "/assets/gifs/javascript/DungeonGeneration.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Fourier": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/fourier", showcase: "/assets/gifs/javascript/Fourier.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Gate Simulation": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/gates_attempt_3", showcase: "/assets/gifs/javascript/Gates.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Pathfinding Visualization": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/pathfinding_visualization", showcase: "/assets/gifs/javascript/Pathfinding.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Slide": { url: "https://github.com/tomster12/coding/tree/master/web/p5js/slide", showcase: "/assets/gifs/javascript/Slide.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["js", "p5js"] },
+    "Boids": { url: "https://github.com/tomster12/coding/tree/master/processing/Boids", showcase: "/assets/gifs/processing/Boids.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Camera": { url: "https://github.com/tomster12/coding/tree/master/processing/Camera", showcase: "/assets/gifs/processing/Camera.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Camera 3D": { url: "https://github.com/tomster12/coding/tree/master/processing/Camera3D", showcase: "/assets/gifs/processing/Camera3D.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Cave Generation": { url: "https://github.com/tomster12/coding/tree/master/processing/CaveGeneration", showcase: "/assets/gifs/processing/CaveGeneration.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Dungeon Generation": { url: "https://github.com/tomster12/coding/tree/master/processing/DungeonGeneration", showcase: "/assets/gifs/processing/DungeonGeneration.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Roguelike Generation": { url: "https://github.com/tomster12/coding/tree/master/processing/RoguelikeGeneration", showcase: "/assets/gifs/processing/RoguelikeGeneration.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Chaos Equations": { url: "https://github.com/tomster12/coding/tree/master/processing/ChaosEquations", showcase: "/assets/gifs/processing/ChaosEquations.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Cut Mesh": { url: "https://github.com/tomster12/coding/tree/master/processing/CutMesh", showcase: "/assets/gifs/processing/CutMesh.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Fluid Sim Main": { url: "https://github.com/tomster12/coding/tree/master/processing/FluidSimMain", showcase: "/assets/gifs/processing/FluidSimulation.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Game Of Life": { url: "https://github.com/tomster12/coding/tree/master/processing/GameOfLife", showcase: "/assets/gifs/processing/GameOfLife.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Gate Simulation": { url: "https://github.com/tomster12/coding/tree/master/processing/GatesAttempt2", showcase: "/assets/gifs/processing/Gates.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Gravity Simulation": { url: "https://github.com/tomster12/coding/tree/master/processing/Gravity", showcase: "/assets/gifs/processing/Gravity.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Lissajous Curves": { url: "https://github.com/tomster12/coding/tree/master/processing/LissajousCurves", showcase: "/assets/gifs/processing/LissajousCurves.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Mandelbrot": { url: "https://github.com/tomster12/coding/tree/master/processing/Mandelbrot", showcase: "/assets/gifs/processing/Mandelbrot.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Marching Squares": { url: "https://github.com/tomster12/coding/tree/master/processing/MarchingSquares", showcase: "/assets/gifs/processing/MarchingSquares.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Maze Generation": { url: "https://github.com/tomster12/coding/tree/master/processing/MazeGeneration", showcase: "/assets/gifs/processing/MazeGeneration.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Sorting": { url: "https://github.com/tomster12/coding/tree/master/processing/Sorting", showcase: "/assets/gifs/processing/SortingAlgorithms.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Particle Clock": { url: "https://github.com/tomster12/coding/tree/master/processing/ParticleClock", showcase: "/assets/gifs/processing/ParticleClock.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Ray Marching 2D": { url: "https://github.com/tomster12/coding/tree/master/processing/RayMarching2D", showcase: "/assets/gifs/processing/RayMarching.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Ray Marching 3D": { url: "https://github.com/tomster12/coding/tree/master/processing/RayMarching3D", showcase: "/assets/gifs/processing/RayMarching3D.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "Ray Marching 2D Shadows": { url: "https://github.com/tomster12/coding/tree/master/processing/RayMarching2DShadows", showcase: "/assets/gifs/processing/RayMarchingShadows.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] },
+    "RB Collision 2D": { url: "https://github.com/tomster12/coding/tree/master/processing/RBCollision2D", showcase: "/assets/gifs/processing/RigidbodyCollision.gif", repo: ["Coding", "https://github.com/tomster12/coding"], tags: ["processing"] }
+};
+
+const tagColours = {
+    "c": "#b9d3f1",
+    "c++": "#d9b7f2",
+    "raylib": "#f1e4b9",
+    "sfml": "#f4acac",
+    "js": "#fff79e",
+    "p5js": "#cfffaf",
+    "processing": "#b9f1d3",
+    "game": "#f1b9b9",
+    "simulation": "#b9f1f1",
+    "repo": "#d9d7d9"
+};
+
+const projectContainer = document.querySelector(".project-container");
+
+function createHTMLElement(elementString) {
+    const div = document.createElement("div");
+    div.innerHTML = elementString;
+    return div.firstElementChild;
+}
+
+function clickProject(e, projectName) {
+    e.stopPropagation();
+    window.location = projects[projectName].url;
+}
+
+function clickProjectRepo(e, projectName) {
+    e.stopPropagation();
+    window.location = projects[projectName].repo[1];
+}
+
+function clickTag(e, tag) {
+    e.stopPropagation();
+}
+
+Object.keys(projects).forEach(projectName => {
+    const project = projects[projectName];
+
+    const projectElement = createHTMLElement(`
+        <div class="project" onclick="clickProject(event, '${projectName}')">
+            <div class="title-section">
+                <p class="title">${projectName}</p>
+            </div>
+            <div class="showcase-section">
+                <img src="${project.showcase}" alt="" />
+            </div>
+            <div class="footer-section">
+                <div class="footer-row">
+                    <p class="label">repo</p>
+                    <span class="tag" style="background-color:${tagColours["repo"]};" onclick="clickProjectRepo(event, '${projectName}')">${project.repo[0]}</span>
+                </div>
+                <div class="footer-row">
+                    <p class="label">tags</p>
+                    <div class="tags-list">
+                        ${project.tags.map(tag => `<span class="tag" style="background-color:${tagColours[tag]};" onclick="clickTag(event, '${tag}')">${tag}</span>`).join("")}
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+    projectContainer.appendChild(projectElement);
+});
